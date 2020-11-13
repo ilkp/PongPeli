b5a2b5c67af5d0ba0bebf953cd1a4a88e4bb2c25
ilkp <ilkka.pokkinen@gmail.com>
2020-11-13 13:21:35 +0200

    Alustava committi

---
+++ Pong2/Assets/Scripts/Pallo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pallo : MonoBehaviour
+{
+    private Rigidbody omaRb;
+    public float nopeus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        omaRb = GetComponent<Rigidbody>();
+        Alusta();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Alusta()
+	{
+        transform.position = Vector3.zero; // tarkoittaa samaa kuin new Vector3(0, 0, 0)
+        float kulma = Random.Range(0, 360);
+        float xSuunta = Mathf.Cos(kulma * Mathf.Deg2Rad);
+        float ySuunta = Mathf.Sin(kulma * Mathf.Deg2Rad);
+        omaRb.velocity = Vector3.zero;
+        omaRb.AddForce(new Vector3(xSuunta, ySuunta, 0) * nopeus, ForceMode.Impulse);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("YlaAlaSeinat"))
+		{
+            Vector3 uusiNopeus = new Vector3(omaRb.velocity.x, omaRb.velocity.y * -1, 0);
+            omaRb.velocity = uusiNopeus;
+		}
+	}
+}
---
+++ Pong2/Assets/Scripts/PelaajanKontrolleri.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PelaajanKontrolleri : MonoBehaviour
+{
+    public float nopeus;
+    public Transform taustaT;
+
+    private float taustanKorkeus;
+    private float omaKorkeus;
+
+	private void Start()
+	{
+        taustanKorkeus = taustaT.localScale.y * 0.5f;
+        omaKorkeus = transform.localScale.y * 0.5f;
+        transform.position = new Vector3(taustaT.localScale.x * -0.5f + 1, 0, 0);
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        // Otetaan pelaajalta syöte
+        float suunta = Input.GetAxisRaw("Vertical") * Time.deltaTime * nopeus;
+        
+
+        if (transform.position.y + suunta < taustanKorkeus - omaKorkeus
+            && transform.position.y + suunta > -taustanKorkeus + omaKorkeus)
+        {
+            transform.Translate(new Vector3(0, suunta, 0));
+        }
+
+    }
+}
